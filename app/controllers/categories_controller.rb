08dff4e5cfdf22dd4721a01dddb4066ff59e214a
nghdung1996 <huydung17101996@gmail.com>
2018-04-19 20:51:07 +0700

    Views All Book,Detail Book, Categories, Authors

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,23 @@
+class CategoriesController < ApplicationController
+  before_action :find_category, only: %i(show)
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @books = category.books.paginate page: params[:page],
+      per_page: Settings.category.max_page
+  end
+
+  private
+
+  attr_reader :category
+
+  def find_category
+    @category = Category.find_by id: params[:id]
+    return if category
+    flash[:danger] = t "category-not-found"
+    redirect_to root_path
+  end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,9 @@ class Book < ApplicationRecord
   has_many :detail_bills
   has_many :rates, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  scope :show_relation_book, (lambda do |id, page|
+    where.not(id: id).paginate page: page,
+      per_page: Settings.relation_book.max_page
+  end)
 end
